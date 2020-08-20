87e3847a5fc1e571a92eed60896462d314c88118
msflyee <1620148368@qq.com>
2020-08-20 09:35:55 +0800

    增加失物招领页面

--- miniprogram/pages/lo_fo/lo_fo.js
+++ miniprogram/pages/lo_fo/lo_fo.js
@@ -1,8 +1,3 @@
-
-Page({}
-
-)
-
 const db = wx.cloud.database({env:'hb-jndbr'});
 
 Page({
@@ -10,7 +5,7 @@ Page({
     TabCur: 0,
     scrollLeft:0,
     selection:["失物","招领"],
-    currentid:1,
+    currentid:0,
     lostlist:[ ],
     foundlist:[ ]
   },
@@ -21,26 +16,46 @@ Page({
       currentid:e.currentTarget.dataset.id
     })
   },
-
+//页面初始化时获取数据列表
   initList:function(){
     var that = this
-    for(let i = 0;i<=5;i++)
+    for(var i = 0;i<=5;i++)  //初始化页面显示条目数量设置为6
     {
+      console.log(i)
       db.collection('lost').doc(String(i))
       .get({
         success:function(res){
+          //将时间戳格式化显示出来
           var lostlist = that.data.lostlist
+          var formatArr = ['Y','M','D']
+          var date = res.data.lost_time
+          var returnArr = [date.getFullYear(),date.getMonth()+1,date.getDate()]
+          var format = "Y年M月D日"
+          for(var j = 0;j<3;j++){
+            format = format.replace(formatArr[j],returnArr[j])
+          }
+          res.data.lost_time = format  //注意：想要更改lost_time，只能先更改res.data的lost_time，不能等到将res.data赋值给lostlist之后再修改
+
           lostlist.push(res.data)
           that.setData({
             lostlist:lostlist
           })
-          console.log("lostlist:",lostlist)
         }
       })
       db.collection('found').doc(String(i))
       .get({
         success:function(res){
           var foundlist = that.data.foundlist
+
+          var formatArr2 = ['Y','M','D']
+          var date2 = res.data.founded_time
+          var returnArr2 = [date2.getFullYear(),date2.getMonth()+1,date2.getDate()]
+          var format2 = "Y年M月D日"
+          for(var k = 0;k<3;k++){
+            format2 = format2.replace(formatArr2[k],returnArr2[k])
+          }
+          res.data.founded_time = format2
+
           foundlist.push(res.data)
           that.setData({
             foundlist:foundlist
@@ -48,13 +63,9 @@ Page({
         }
       })
     }
-    console.log("lostlist:"+that.data.lostlist)
- 
-    for(var i = 0;i<=1;i++)
-    {
-      console.log(that.data.lostlist[i])
-    }
+    console.log(that.data.lostlist)
   },
+
   getList:function(){
     
   },
